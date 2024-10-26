9f4955dee586e245257dc3377f8a7b92d9367db5
alandiego <diego.uft@gmail.com>
2024-10-26 00:54:18 -0300

    Create 1011.cpp

---
+++ 1011.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main() {
+    double r, pi=3.14159, vol;
+    
+    scanf("%lf", &r);
+
+    vol = (4.0/3.0) * pi * (r*r*r);
+    
+    printf("VOLUME = %.3lf\n", vol); 
+    return 0;
+}
