686fbf812a990de5ec9b30f3106992cfb5b78ab1
alandiego <diego.uft@gmail.com>
2025-03-18 00:06:33 -0300

    Create 2413.c

---
+++ 2413.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ 
+int main() {
+    int primeiro;
+    int segundo;
+    int terceiro;
+    
+    
+    scanf("%d", &terceiro);
+    
+    segundo = terceiro*2;
+    
+    primeiro = segundo*2;
+ 
+    printf("%d\n",primeiro);
+
+ 
+    return 0;
+}
