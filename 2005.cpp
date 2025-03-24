985a7dd0db83c970f1de78537f2d33cdce929c03
alandiego <diego.uft@gmail.com>
2025-03-24 20:34:55 -0300

    Create 2005.cpp

---
+++ 2005.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool comp(int a, int b) {
+    return a > b;
+}
+
+int main()
+{
+    int N; //qtd de ratos
+    int M; //qtd de alimentos
+    
+    vector<int> rato;
+    vector<int> sustento;
+    
+    int temp;
+    
+    int qtd_ratos_saciados;
+    int achou;
+    
+    
+    while (cin >> N >> M){
+        rato.assign( 1001, 0);
+        sustento.assign(M, 0);    
+        qtd_ratos_saciados =0;
+        
+        for (int i=0; i<N; i++){
+            cin >> temp;
+            rato[temp]++;
+        }
+        
+        for (int i=0; i<M; i++){
+            cin >> sustento[i];
+        }
+        
+        //loop p decidir para que rato vai o alimento
+        for (int i=0; i<M; i++){
+            int achou = false; // verifica se tem um rato que mata a fome com essa comida
+            for (int j= sustento[i]; j>0; j--){
+                if (rato[j] > 0){
+                    rato[j]--;
+                    qtd_ratos_saciados++;
+                    achou=true;
+                    break;
+                }
+            }
+            if (!achou){
+                //procura de que rato diminuir a fome
+                for (int j=1000; j>0; j--){
+                    if (rato[j] > 0){
+                        rato[j]--;
+                        rato[j-sustento[i]]++;
+                        break;
+                    }
+                }
+            }
+            
+        }
+        
+        cout << qtd_ratos_saciados << endl;
+    }
+
+    return 0;
+}
