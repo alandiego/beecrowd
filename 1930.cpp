957975d487593b20db9b1850ce0594da2e1a8330
alandiego <diego.uft@gmail.com>
2025-03-31 10:48:16 -0300

    Create 1930.cpp

---
+++ 1930.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int soma = 0;
+    int temp;
+    
+    while (cin >> temp)
+        soma +=temp;
+        
+    cout << soma-3 << endl;
+    
+    return 0;
+}
