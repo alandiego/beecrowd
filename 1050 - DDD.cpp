e98265e5338b92b4bd5a5148e0e049b7d8f27f9a
alandiego <diego.uft@gmail.com>
2025-05-03 02:35:07 -0300

    Create 1050 - DDD.cpp

---
+++ 1050 - DDD.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios_base :: sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int DDD;
+	
+	cin >> DDD;
+	
+	if (DDD == 61)
+	    cout  <<"Brasilia\n";
+	else if (DDD == 71)
+	    cout  <<"Salvador\n";
+	else if (DDD == 11)
+	    cout  <<"Sao Paulo\n";
+    else if (DDD == 21)
+	    cout  <<"Rio de Janeiro\n";
+	else if (DDD == 32)
+	    cout  <<"Juiz de Fora\n";
+	else if (DDD == 19)
+	    cout  <<"Campinas\n";
+    else if (DDD == 27)
+	    cout  <<"Vitoria\n";
+	else if (DDD == 31)
+	    cout  <<"Belo Horizonte\n";
+	else
+        cout  <<"DDD nao cadastrado\n";
+        
+    return 0;
+}
