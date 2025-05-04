5d2ad04f3a6fae32d102cf058ce2e8e0aa7c3606
alandiego <diego.uft@gmail.com>
2025-05-04 00:45:41 -0300

    Create 1067 - Números Ímpares.cpp

---
+++ 1067 - Números Ímpares.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base :: sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int X;
+    cin >> X;
+
+    for (int i = 1; i <= X; i += 2) {
+        cout << i << "\n";
+    }
+
+    return 0;
+}
