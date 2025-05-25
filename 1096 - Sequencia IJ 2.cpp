e168fe6854538f60febe9ae6eeb1f597f092b8a5
alandiego <diego.uft@gmail.com>
2025-05-25 10:28:47 -0300

    Create  1096 - Sequencia IJ 2.cpp

---
+++ 1096 - Sequencia IJ 2.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base :: sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int I, J;
+    
+    for (I=1; I<10; I+=2){
+        for (J=7; J>=5; --J)
+            cout << "I=" << I << " J=" << J <<"\n";
+    }
+        
+        
+    return 0;
+}
