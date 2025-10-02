215a97109bfbb1e45ab8f56765fb96aa690897b8
alandiego <diego.uft@gmail.com>
2025-10-02 00:08:07 -0300

    Create 1387 - Og.cpp

---
+++ 1387 - Og.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base :: sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int L, R;
+    
+    while (cin >> L >> R && L && R){
+        cout << L+R << "\n";
+    }
+ 
+    return 0;
+}
