4bb24bfb3c341ed73cf50ed453e37d4f2b644f0c
alandiego <diego.uft@gmail.com>
2026-01-28 22:43:58 -0300

    Update 1579 - Transporte de Painéis Solares.cpp

--- 1579 - Transporte de Painéis Solares.cpp
+++ 1579 - Transporte de Painéis Solares.cpp
@@ -1,60 +1,47 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-// verifica se é possível transportar os painéis
-// com no máximo 'caminhoes', sem ultrapassar 'peso'
-bool carrega(const vector<int> &paineis, int peso, int caminhoes) {
-    int pesoAtual = 0;
 
-    for (int p : paineis) {
-        if (pesoAtual + p <= peso) {
-            pesoAtual += p;
-        } else {
-            pesoAtual = p;
+//verifica se da pra carregar os caminhoes com esse peso
+bool carrega(vector<int> &paineis, int peso, int caminhoes){
+    int pesoatual=0;
+    for (auto &p : paineis){
+        if (pesoatual+p <= peso)
+            pesoatual+=p;
+        else {
+            pesoatual=p;
             caminhoes--;
-            if (caminhoes == 0)
+            if (caminhoes==0)
                 return false;
         }
     }
     return true;
 }
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-
-    int N;
+int main (){
+    int N, nPaineis, caminhoes, frete;
+    
     cin >> N;
-
-    while (N--) {
-        int nPaineis, caminhoes, frete;
+    while(N--){
         cin >> nPaineis >> caminhoes >> frete;
-
+        
         vector<int> paineis(nPaineis);
-        for (int i = 0; i < nPaineis; i++) {
+        
+        for(int i=0; i<nPaineis; i++){
             cin >> paineis[i];
         }
-
-        //menor peso possível
-        int lo = *max_element(paineis.begin(), paineis.end());
-        //maior peso possível
-        int hi = accumulate(paineis.begin(), paineis.end(), 0LL);
-
-        // binary search
-        while (lo < hi) {
-            int mid = (lo + hi) / 2;
-            if (carrega(paineis, mid, caminhoes))
-                hi = mid;
-            else
-                lo = mid + 1;
+        
+        long long lo = *max_element(paineis.begin(), paineis.end());
+        long long hi = accumulate(paineis.begin(), paineis.end(),0);
+        while (lo<hi){
+            long long mid = (lo+hi)/2;
+            carrega(paineis, mid, caminhoes) ? hi=mid : lo = mid+1;
         }
-
-        int pesoMax = lo;
-        int custo = pesoMax * frete * caminhoes;
-
-        cout << pesoMax << " $" << custo << "\n";
+        
+        cout << lo << " $" << hi*frete*caminhoes << '\n';  
+        
     }
-
+    
     return 0;
 }
