07b1281e1fe82f5bfcab998e7d18aa6c33387596
alandiego <diego.uft@gmail.com>
2026-01-29 22:02:37 -0300

    Create  2454 - Flíper.cpp

---
+++ 2454 - Flíper.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int P, R;
+    cin >> P >> R;
+    
+    if (P == 0)
+        cout << "C\n";
+    else {
+        if (R == 1)
+            cout << "A\n";
+        else
+            cout << "B\n";
+    }
+    
+    return 0;
+}
