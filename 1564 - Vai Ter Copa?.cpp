4a85091fc77d81487f0fa65dca74060994d19acf
alandiego <diego.uft@gmail.com>
2026-02-18 01:10:16 -0300

    Create 1564 - Vai Ter Copa?.cpp

---
+++ 1564 - Vai Ter Copa?.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios_base :: sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int N;
+	
+	while (cin >> N){
+        if (N)
+		    cout << "vai ter duas!\n";
+		else
+		    cout << "vai ter copa!\n";
+	}
+	
+	return 0;
+}
