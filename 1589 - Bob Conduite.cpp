7da52407d0f4b291bbe8c9255bd6e839b077d317
alandiego <diego.uft@gmail.com>
2026-04-21 23:23:30 -0300

    Create  1589 - Bob Conduite.cpp

---
+++ 1589 - Bob Conduite.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int T, R1, R2;
+    
+    cin >> T;
+    
+    while (T--){
+        cin >> R1 >> R2;
+        cout << R1+R2 << endl;
+    }
+    
+    return 0;
+}
