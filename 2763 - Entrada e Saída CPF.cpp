6ef578c37b243795a65f475f906649d0ce29357a
alandiego <diego.uft@gmail.com>
2026-05-27 20:43:42 -0300

    Create  2763 - Entrada e Saída CPF.cpp

---
+++ 2763 - Entrada e Saída CPF.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a, b, c, d;
+
+    while (scanf("%d%*c%d%*c%d%*c%d", &a, &b, &c, &d) != EOF){
+        printf("%03d\n", a);
+        printf("%03d\n", b);
+        printf("%03d\n", c);
+        printf("%02d\n", d);
+    }
+
+    return 0;
+}
